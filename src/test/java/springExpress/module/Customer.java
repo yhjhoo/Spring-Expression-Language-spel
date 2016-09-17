496983a703c50d2313979372aa563362ba58d5bd
yhjhoo <yhjhoo@163.com>
2016-09-17 10:57:58 +0800

    init

---
+++ src/test/java/springExpress/module/Customer.java
@@ -0,0 +1,18 @@
+package springExpress.module;
+
+public class Customer {
+	private Item item;
+	private String itemName;
+	public Item getItem() {
+		return item;
+	}
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+}
---
+++ src/test/java/springExpress/module/Item.java
@@ -0,0 +1,19 @@
+package springExpress.module;
+
+public class Item {
+	private String name;
+	private int total;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+}
